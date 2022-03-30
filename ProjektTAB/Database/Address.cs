cdd4a9cac78042ae39709bc8fe4d05fb0e4c2022
Jerzy <jurekbalcer@gmail.com>
2022-03-30 20:48:22 +0200

    house and room number are now strings with limited length

--- ProjektTAB/Database/Address.cs
+++ ProjektTAB/Database/Address.cs
@@ -1,14 +1,20 @@
-﻿namespace Database
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Database
 {
     public class Address
     {
         public int AddressId { get; set; }
         public string City { get; set; }
         public string Street { get; set; }
-        public int HouseNumber { get; set; }
-        public int? RoomNumber { get; set; }
 
-        public Address(string city, string street, int houseNumber, int? roomNumber)
+        [MaxLength(10)]
+        public string HouseNumber { get; set; }
+
+        [MaxLength(10)]
+        public string? RoomNumber { get; set; }
+
+        public Address(string city, string street, string houseNumber, string? roomNumber)
         {
             City = city;
             Street = street;
