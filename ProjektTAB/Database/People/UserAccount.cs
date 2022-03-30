8eda611891107b41083caa90d5b3fc5aabf75f60
Jerzy <jurekbalcer@gmail.com>
2022-03-30 20:50:35 +0200

    IsActive property in UserAccount

--- ProjektTAB/Database/People/UserAccount.cs
+++ ProjektTAB/Database/People/UserAccount.cs
@@ -6,6 +6,7 @@
         public string Email { get; set; }
         public string Password { get; set; }
         public User User { get; set; }
+        public bool IsActive { get; set; }
 
         public UserAccount(string email, string password)
         {
