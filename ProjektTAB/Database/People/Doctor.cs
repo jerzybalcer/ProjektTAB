2edbcd70a48f2c08394b357633f31d94dbf61458
Jerzy <jurekbalcer@gmail.com>
2022-03-30 21:07:09 +0200

    license number max length

--- ProjektTAB/Database/People/Doctor.cs
+++ ProjektTAB/Database/People/Doctor.cs
@@ -1,7 +1,10 @@
-﻿namespace Database.People
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Database.People
 {
     public class Doctor : User
     {
+        [MaxLength(7)]
         public int LicenseNumber { get; set; }
 
         public Doctor(string name, string surname, UserAccount userAccount, int licenseNumber) : base(name, surname, userAccount)
