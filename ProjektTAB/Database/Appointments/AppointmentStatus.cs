2cd6517fd77260d5e6b8f615ab9d5e38689ace03
Jerzy <jurekbalcer@gmail.com>
2022-03-30 21:47:05 +0200

    improved status enums

--- ProjektTAB/Database/Appointments/AppointmentStatus.cs
+++ ProjektTAB/Database/Appointments/AppointmentStatus.cs
@@ -2,9 +2,11 @@
 {
     public enum AppointmentStatus
     {
-        Attended,
-        Unattended,
-        Registered,
-        Failed
+        Registered, // appointment will happen in the future
+        Started,    // appointment started but not ended yet
+        Finished,   // successfully ended
+        Unattended, // patient didn't come
+        Cancelled,  // patient asked to cancel it
+        Failed      // patient came but doctor was unable to examine the patient
     }
 }
--- ProjektTAB/Database/Examinations/LabExaminationStatus.cs
+++ ProjektTAB/Database/Examinations/LabExaminationStatus.cs
@@ -2,9 +2,10 @@
 {
     public enum LabExaminationStatus
     {
-        Ordered,
-        Executed,
-        Accepted,
-        Failed,
+        Ordered,                // examination ordered by doctor, waits to be done
+        SuccessfullyExecuted,   // done by assistant, waits for manager to check it
+        CancelledByAssistant,   // there was some error during examination, couldn't finish
+        AcceptedByManager,      // manager accepted it, finished successfully
+        RejectedByManager,      // manager cancelled it
     }
 }
