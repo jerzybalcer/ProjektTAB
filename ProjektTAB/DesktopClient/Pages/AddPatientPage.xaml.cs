28304b523b7dfd317eaed224873bea871d022165
Jan Staniszewski <56159109+JanStaniszewski@users.noreply.github.com>
2022-04-04 19:01:49 +0200

    AddPatient Page edit and begining of api call

--- ProjektTAB/DesktopClient/Pages/AddPatientPage.xaml.cs
+++ ProjektTAB/DesktopClient/Pages/AddPatientPage.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Database;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -26,10 +29,36 @@ namespace DesktopClient.Pages
             InitializeComponent();
         }
 
-        private void AddPatientBtn_Click(object sender, RoutedEventArgs e)
+        private async void AddPatientBtn_Click(object sender, RoutedEventArgs e)
         {
+            // call authentication api
+            HttpClient client = new HttpClient();
 
+
+            var values = new Dictionary<string, string>
+            {
+                { "name", firstname.Text },
+                { "surname", lastname.Text },
+                { "pesel", pesel.Text },
+                { "city", city.Text },
+                { "street", street.Text },
+                { "houseNumber", house.Text },
+                { "roomNumber", apartment.Text }
+            };
+
+            //string content = JsonConvert.SerializeObject(values);
+            var content = new FormUrlEncodedContent(values);
+
+            var response = await client.PostAsync("https://tabbackend.azurewebsites.net/", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseString = await response.Content.ReadAsStringAsync();
+                var jsonSettings = JsonConfiguration.GetJsonSettings();
+
+            }
         }
+
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
             Regex regex = new Regex("[^0-9]+");
