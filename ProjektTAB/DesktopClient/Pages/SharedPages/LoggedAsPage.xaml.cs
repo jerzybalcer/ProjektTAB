58a598b6d1b067f5cf3d8a83b1741715193d5f6d
Jerzy <jurekbalcer@gmail.com>
2022-04-14 23:05:09 +0200

    fancy homepage

--- ProjektTAB/DesktopClient/Pages/SharedPages/LoggedAsPage.xaml.cs
+++ ProjektTAB/DesktopClient/Pages/SharedPages/LoggedAsPage.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using Database.Users;
 using Database.Users.Simplified;
 using DesktopClient.Authentication;
+using System;
+using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Threading;
 
 namespace DesktopClient.Pages.SharedPages
 {
@@ -12,16 +15,13 @@ namespace DesktopClient.Pages.SharedPages
     public partial class LoggedAsPage : Page
     {
         private readonly UserSimplified? _loggedUser = null;
-
-        public LoggedAsPage()
-        {
-            InitializeComponent();
-        }
+        private readonly DispatcherTimer _timer;
 
         public LoggedAsPage(UserSimplified loggedUser)
         {
             InitializeComponent();
             _loggedUser = loggedUser;
+            _timer = new DispatcherTimer();
         }
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
@@ -34,6 +34,25 @@ namespace DesktopClient.Pages.SharedPages
             {
                 LoggedUserName.Text = "nie zalogowano";
             }
+
+            ClockTick(null, null);
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += ClockTick;
+            _timer.Start();
+        }
+
+        private void ClockTick(object? sender, EventArgs e)
+        {
+            ClockHours.Text = DateTime.Now.Hour.ToString("D2");
+            ClockMinutes.Text = DateTime.Now.Minute.ToString("D2");
+            ClockSeconds.Text = DateTime.Now.Second.ToString("D2");
+
+            var culture = new CultureInfo("pl-PL");
+
+            WeekDay.Text = ((int)DateTime.Now.DayOfWeek).ToString(culture.DateTimeFormat.GetDayName(DateTime.Now.DayOfWeek), culture);
+            Day.Text = DateTime.Now.Day.ToString();
+            Month.Text = ((int)DateTime.Now.Month).ToString(culture.DateTimeFormat.GetMonthName(DateTime.Now.Month), culture);
+            Year.Text = DateTime.Now.Year.ToString();
         }
 
         private void LogoutButton_Click(object sender, RoutedEventArgs e)
@@ -41,6 +60,11 @@ namespace DesktopClient.Pages.SharedPages
             CurrentAccount.Logout();
 
             NavigationService.Navigate(new LoginPage());
+        }
+
+        private void Page_Unloaded(object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
         }
     }
 }
