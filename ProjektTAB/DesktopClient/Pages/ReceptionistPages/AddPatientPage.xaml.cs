dfd583a4d7921e35c2bef4d7c2011106c69e2e53
Jerzy <jurekbalcer@gmail.com>
2022-04-15 16:12:23 +0200

    add patient page more cosmetics

--- ProjektTAB/DesktopClient/Pages/ReceptionistPages/AddPatientPage.xaml.cs
+++ ProjektTAB/DesktopClient/Pages/ReceptionistPages/AddPatientPage.xaml.cs
@@ -25,15 +25,41 @@ namespace DesktopClient.Pages.ReceptionistPages
             var response = await ApiCaller.Post("AddPatient", newPatient);
 
             if (response.IsSuccessStatusCode)
-                MessageBox.Show("Poprawnie dodano pacjenta");
+            {
+                MessageBox.Show("Poprawnie dodano pacjenta!");
+                NavigationService.Navigate(new AddPatientPage());
+            }
             else
-                MessageBox.Show("Nie dodano pacjenta");
+            {
+                MessageBox.Show("Wystąpił błąd podczas dodawania pacjenta!");
+            }
         }
 
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
             Regex regex = new Regex("[^0-9]+");
             e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            AddPatientBtn.IsEnabled = true;
+
+            foreach (var element in InputPanel.Children)
+            {
+                if (element is TextBox)
+                {
+                    TextBox textBox = (TextBox)element;
+
+                    if ((textBox.Name != "apartment"
+                        && textBox.Text.Length == 0)
+                        || (textBox.Name == "pesel" && textBox.Text.Length < 11)
+                        )
+                    {
+                        AddPatientBtn.IsEnabled = false;
+                    }
+                }
+            }
         }
     }
 }
